f534a0f65a0eabc378ddb3350b8bc5586761556c
SkybaMaryna <122438581+SkybaMaryna@users.noreply.github.com>
2023-03-22 15:06:48 +0000

    Deploying to gh-pages from @ SkybaMaryna/goit-js-hw-09@5cf42dc66bc6675c8da2726f32f38031d0296809 🚀

---
+++ 01-color-switcher.25ebe99c.js
@@ -0,0 +1,2 @@
+!function(){var t={body:document.querySelector("body"),startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]")},n=null;t.stopBtn.disabled=!0,t.startBtn.addEventListener("click",(function(){t.stopBtn.disabled=!1,t.startBtn.disabled=!0,n=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),t.stopBtn.addEventListener("click",(function(){t.startBtn.disabled=!1,t.stopBtn.disabled=!0,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.25ebe99c.js.map
