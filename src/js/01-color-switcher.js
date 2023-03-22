5cf42dc66bc6675c8da2726f32f38031d0296809
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-03-22 17:06:18 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+}
+let IntervalId = null
 
+refs.stopBtn.disabled = true
+
+refs.startBtn.addEventListener('click', onChangeColor)
+refs.stopBtn.addEventListener('click', onStopChangeColor)
+
+function onChangeColor() {
+    refs.stopBtn.disabled = false
+    refs.startBtn.disabled = true
+
+    IntervalId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+}
+
+function onStopChangeColor() {
+    refs.startBtn.disabled = false
+    refs.stopBtn.disabled = true
+    clearInterval(IntervalId)
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
