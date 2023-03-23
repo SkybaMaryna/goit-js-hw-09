8b3a7dc2c42c894addb1c60874160e4163d5aa58
Your Name~ <122438581+SkybaMaryna@users.noreply.github.com>
2023-03-23 15:40:49 +0200

    update 03-promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form')
+}
+
+refs.form.addEventListener('submit', onCreatePromises)
+
+function onCreatePromises(event) {
+  event.preventDefault();
+  const amount = event.target.elements.amount.value
+  let delay = Number(event.target.elements.delay.value)
+  const step = Number(event.target.elements.step.value)
+  
+  let position = 1
+  for (let i = 0; i < amount; i++) {
+    
+    createPromise(position, delay)
+    delay += step
+    position += 1
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`)
+      }
+    }, delay)
+  })
+    .then((message) => Notiflix.Notify.success(message))
+    .catch((error) => Notiflix.Notify.failure(error))
 }
